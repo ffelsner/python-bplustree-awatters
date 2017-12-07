8345564f6a5a32f0c5c38fc6a4df9c78b7963d71
Daniel Walton <dwalton76@gmail.com>
2017-12-07 08:42:46 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='bplustree',
+    version='1.0.0',
+    description='B+ Tree in Python',
+    keywords='btree bplustree b+tree',
+    url='https://github.com/dwalton76/bplustree',
+    author='Daniel Walton',
+    author_email='dwalton76@gmail.com',
+    license='GPLv3',
+    packages=['bplustree'],
+)
+
